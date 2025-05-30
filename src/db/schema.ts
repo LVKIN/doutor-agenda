4c83382a981c27e1546f2f07391e17f7731f5692
LVKIN <backuplucas6150@gmail.com>
2025-05-30 10:05:46 -0300

    chore: better auth setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,28 +1,70 @@
 import { relations } from "drizzle-orm";
-import { uuid, integer, pgTable, text, varchar, timestamp, time, pgEnum } from "drizzle-orm/pg-core";
+import { uuid, integer, pgTable, text, varchar, timestamp, time, pgEnum, boolean } from "drizzle-orm/pg-core";
 
-export const userTable = pgTable("users", {
-    id: uuid("id").defaultRandom().primaryKey()
+export const usersTable = pgTable("users", {
+    id: text('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    emailVerified: boolean('email_verified').$defaultFn(() => false).notNull(),
+    image: text('image'),
+    createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull(),
+    updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
 });
 
-export const userTableRelations = relations(userTable, ({ many }) => ({
-    usersToClinics: many(userToClinicsTable)
+export const sessionsTable = pgTable("session", {
+    id: text('id').primaryKey(),
+    expiresAt: timestamp('expires_at').notNull(),
+    token: text('token').notNull().unique(),
+    createdAt: timestamp('created_at').notNull(),
+    updatedAt: timestamp('updated_at').notNull(),
+    ipAddress: text('ip_address'),
+    userAgent: text('user_agent'),
+    userId: text('user_id').notNull().references(() => usersTable.id, { onDelete: 'cascade' })
+});
+
+export const accountsTable = pgTable("account", {
+    id: text('id').primaryKey(),
+    accountId: text('account_id').notNull(),
+    providerId: text('provider_id').notNull(),
+    userId: text('user_id').notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
+    accessToken: text('access_token'),
+    refreshToken: text('refresh_token'),
+    idToken: text('id_token'),
+    accessTokenExpiresAt: timestamp('access_token_expires_at'),
+    refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+    scope: text('scope'),
+    password: text('password'),
+    createdAt: timestamp('created_at').notNull(),
+    updatedAt: timestamp('updated_at').notNull()
+});
+
+export const verificationsTable = pgTable("verification", {
+    id: text('id').primaryKey(),
+    identifier: text('identifier').notNull(),
+    value: text('value').notNull(),
+    expiresAt: timestamp('expires_at').notNull(),
+    createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
+    updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
+});
+
+export const usersTableRelations = relations(usersTable, ({ many }) => ({
+    usersToClinics: many(usersToClinicsTable)
 }));
 
-export const userToClinicsTable = pgTable("user_to_clinics", {
-    userId: uuid("user_id").notNull().references(() => userTable.id, { onDelete: "cascade" }),
+export const usersToClinicsTable = pgTable("user_to_clinics", {
+    userId: text("user_id").notNull().references(() => usersTable.id, { onDelete: "cascade" }),
     clinicId: uuid("clinic_id").notNull().references(() => clinicsTable.id, { onDelete: "cascade" }),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().$onUpdate(() => new Date())
 });
 
-export const userToClinicsTableRelations = relations(userToClinicsTable, ({ one }) => ({
-    user: one(userTable, {
-        fields: [userToClinicsTable.userId],
-        references: [userTable.id]
+export const usersToClinicsTableRelations = relations(usersToClinicsTable, ({ one }) => ({
+    user: one(usersTable, {
+        fields: [usersToClinicsTable.userId],
+        references: [usersTable.id]
     }),
     clinic: one(clinicsTable, {
-        fields: [userToClinicsTable.clinicId],
+        fields: [usersToClinicsTable.clinicId],
         references: [clinicsTable.id]
     })
 }));
@@ -38,7 +80,7 @@ export const clinicsTableRelations = relations(clinicsTable, ({ many }) => ({
     doctors: many(doctorsTable),
     patients: many(patientsTable),
     appointments: many(appointmentsTable),
-    usersToClinics: many(userToClinicsTable)
+    usersToClinics: many(usersToClinicsTable)
 }))
 
 export const doctorsTable = pgTable("doctors", {
